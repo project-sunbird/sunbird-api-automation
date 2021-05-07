8768e82bc99b092d1e47cf2aba05918bc6da572e
binayakdash123 <64831476+binayakdash123@users.noreply.github.com>
2021-05-07 16:00:54 +0530

    Update Utils.java

--- src/test/java/resources/Utils.java
+++ src/test/java/resources/Utils.java
@@ -19,7 +19,7 @@ public class Utils {
 	static Properties prop;
 	public static RequestSpecification req;
 	JSONParser parser = new JSONParser();
-	Object obj = parser.parse(new FileReader("C:/Users/admin/Desktop/testData.json"));
+	Object obj = parser.parse(new FileReader("C:/Jenkinspath/testData.json"));
 	JSONObject jsonObject = (JSONObject) obj;
 	public  String baseuri=(String)jsonObject.get("baseUrl");
 	public  String auth_key=(String)jsonObject.get("authToken");
